a50a5e409cbe8ae9fd04fce7703f8f2c86898e95
Agung Sepruloh <agungsepruloh1996@gmail.com>
2019-09-24 19:13:34 +0700

    Cara melakukan operasi aritmatika

---
+++ src/com/pbo/PBO11K10118902Latihan12Aritmatika.java
@@ -0,0 +1,32 @@
+package com.pbo;
+
+/**
+ *
+ * # Author
+ * Nama : Agung Sepruloh
+ * Kelas : PBO11K
+ * NIM : 10118902
+ * Deskripsi Program : Program ini berisi cara melakukan operasi aritmatika
+ *
+ */
+
+public class PBO11K10118902Latihan12Aritmatika {
+
+    public static void main(String[] args) {
+	    int a = 10;
+	    int b = 12;
+	    int c = 25;
+	    int d = 25;
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (b--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+    }
+}
